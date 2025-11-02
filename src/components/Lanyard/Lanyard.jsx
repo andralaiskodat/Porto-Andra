50b3a5094715996b6d40f20db1123bdfd322ee5a
andralaiskodat <andraqbz@gmail.com>
2025-11-02 18:24:44 +0700

    comit baru

--- src/components/Lanyard/Lanyard.jsx
+++ src/components/Lanyard/Lanyard.jsx
@@ -10,7 +10,7 @@ import * as THREE from 'three';
 // 🧩 Ganti import glb menjadi path dari public/
 const cardGLB = '/models/card.glb';
 // 🧩 Tetap bisa pakai png dari src
-import lanyard from '../../assets/Lanyard/lanyard.png';
+import lanyard from '../../assets/Lanyard/Lanyrd.png';
 
 extend({ MeshLineGeometry, MeshLineMaterial });
 
